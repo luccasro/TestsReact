ff30fd51ac6a3e5c83c29cd4fa4ea64f5c9bc70b
Luccas Rodrigues <luccasro98@gmail.com>
2021-01-12 22:35:21 -0300

    first

---
+++ fundamentos/src/Components/basicos/Primeiro.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Primeiro() {
+    const msg = 'Usuário';
+    return (
+    
+    <div>
+         <h1 className="lbl">Bem-Vindo {msg} </h1>
+    </div>
+    )
+}
---
+++ fundamentos/src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import './index.css'
+import Primeiro from './Components/basicos/Primeiro'
+
+const el = document.getElementById('root');
+ReactDOM.render(
+    <Primeiro></Primeiro>
+    
+    
+    , el);
